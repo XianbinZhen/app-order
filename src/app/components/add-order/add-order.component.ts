26651871366b8c3129cff4386867102803cbe21c
XianbinZhen <62436049+XianbinZhen@users.noreply.github.com>
2021-03-10 23:40:50 -0500

    add multiple tables

--- src/app/components/add-order/add-order.component.ts
+++ src/app/components/add-order/add-order.component.ts
@@ -14,30 +14,46 @@ export class AddOrderComponent implements OnInit {
   foodPrice:number = 0;
   defaultMenu:Food[] = [];
   order:Food[] = [];
+  tableNumber: number = 0;
+  tableTotal: number = 0;
 
   constructor(private foodService:FoodService) {
-    this.order = this.foodService.order;
     this.defaultMenu = this.foodService.defaultMenu;
+    this.tableNumber = this.foodService.tableNumber;
+    this.order = this.foodService.table[this.tableNumber];
+    this.tableTotal = this.foodService.table.length;
   }
 
   ngOnInit(): void {
   }
   
-  addCustomFood(): void{
+  addCustomFood(): void {
     let newFood:Food = new Food(this.foodName, this.foodPrice);
-    this.foodService.order.push(newFood);
-    this.order = this.foodService.order;
+    this.foodService.table[this.tableNumber].push(newFood);
+    this.order = this.foodService.table[this.tableNumber];
   }
   
-  addFood(food:Food): void{
-    this.foodService.order.push(food);
-    this.order = this.foodService.order;
+  addFood(food:Food): void {
+    this.foodService.table[this.tableNumber].push(food);
+    this.order = this.foodService.table[this.tableNumber];
   }
 
   removeFood(index:number): void {
-    this.foodService.order.splice(index, 1);
+    this.foodService.table[this.tableNumber].splice(index, 1);
   }
 
+  addTable(): void {
+    this.foodService.table[this.foodService.tableTotal] = [];
+    this.foodService.tableNumber = this.foodService.tableTotal;
+    this.foodService.tableTotal++;
+    this.order = [];
+    this.tableNumber = this.foodService.tableNumber;
+    this.tableTotal = this.foodService.tableTotal;
+  }
 
+  changeTable(num:number): void {
+    this.tableNumber = num;
+    this.order = this.foodService.table[this.tableNumber];
+  }
 
 }
